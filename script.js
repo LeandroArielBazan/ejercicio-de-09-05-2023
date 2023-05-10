c07bf56d58a66c19ebcb9a3572443379a3f0d49b
Leandro Ariel Bazan <leanbazan99@gmail.com>
2023-05-10 16:48:23 -0300

    agregue boton de reinicio de clicks a la pagina

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+const pContador = document.querySelector ("#contador")
 
-
+let contador = 0;
 
 function aumentaContador(){
-    
+        contador = contador +1;
+        pContador.innerHTML = contador;
+}
+
+
+function reiniciarContador(){
+    contador = 0;
+    pContador.innerHTML = contador; 
 }
